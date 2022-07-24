5391fa86ea4e912371987a1ab07aa2ad333d0a2b
Emmanuel <emma_ajoku2011@hotmail.com>
2022-07-24 02:25:49 +0100

    added custum migration

--- database/migrations/2021_10_02_190207_create_cart_shippings_table.php
+++ database/migrations/2021_10_02_190207_create_cart_shippings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateCartShippingsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('cart_shippings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('cart_group_id')->nullable();
+            $table->bigInteger('shipping_method_id')->nullable();
+            $table->float('shipping_cost')->default(0);
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('cart_shippings');
     }
 }
