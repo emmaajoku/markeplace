b47008c70ffcc948ba25fd0061999002a44b41a8
Emmanuel <emma_ajoku2011@hotmail.com>
2022-07-24 04:39:37 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,21 +31,6 @@
                 </div>
             @endif
 
-            <form action="{{ route('aws-upload') }}" method="POST" enctype="multipart/form-data">
-                @csrf
-                <div class="row">
-
-                    <div class="col-md-6">
-                        <input type="file" name="image" class="form-control">
-                    </div>
-
-                    <div class="col-md-6">
-                        <button type="submit" class="btn btn-success">Upload</button>
-                    </div>
-
-                </div>
-            </form>
-
         </div>
     </div>
 </div>
