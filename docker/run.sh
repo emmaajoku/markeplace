1c311429dc94790d5455fe308094c6614dd3151a
Emmanuel <emma_ajoku2011@hotmail.com>
2022-07-26 19:51:04 +0100

    update

--- docker/run.sh
+++ docker/run.sh
@@ -5,6 +5,7 @@ cd /var/www
 # php artisan migrate:fresh --seed
 php artisan cache:clear
 php artisan route:cache
+php artisan storage:link
 chmod -R 777 /var/www/storage/
 chmod -R 777 /var/www/.env
 chmod -R 777 /var/www/app/Providers/RouteServiceProvider.php
