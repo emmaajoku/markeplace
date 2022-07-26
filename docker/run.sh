660e1194d64ae2e87372a97ce2e0d857a8c6b427
Emmanuel <emma_ajoku2011@hotmail.com>
2022-07-26 18:52:52 +0100

    update

--- docker/run.sh
+++ docker/run.sh
@@ -9,4 +9,5 @@ chmod -R 777 /var/www/storage/
 chmod -R 777 /var/www/.env
 chmod -R 777 /var/www/app/Providers/RouteServiceProvider.php
 chmod -R 777 /var/www/bootstrap/
+chmod -R 775 /var/www/public/
 /usr/bin/supervisord -c /etc/supervisord.conf
