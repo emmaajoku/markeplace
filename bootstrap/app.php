1cde7a9fa195fbaa1fdf7c869f545a8d15236aeb
Emmanuel <emma_ajoku2011@hotmail.com>
2022-07-27 10:39:43 +0100

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -53,7 +53,7 @@ $app->singleton(
 */
 
 /*header('Access-Control-Allow-Origin: *');*/
-header('Access-Control-Allow-Methods: *');
-header('Access-Control-Allow-Headers: *');
+// header('Access-Control-Allow-Methods: *');
+// header('Access-Control-Allow-Headers: *');
 
 return $app;
